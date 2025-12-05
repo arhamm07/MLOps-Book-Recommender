43d06aecd6a85c141d29b356ef0f3db3d346bc93
arham <arhamop937@gmail.com>
2025-12-05 19:21:22 +0500

    script to create a local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="books_recommender",
+    version="0.0.1",
+    author="M Arham Rafique",
+    author_email="arhammrafique@gmail.com",
+    packages=find_packages()
+)
